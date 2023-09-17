198249be6a993dcf8c71114dca22b34dfee96c5e
Denis Viktorovich <slbdenis.victor@gmail.com>
2023-09-17 14:12:39 +0300

    server PlayerServer

---
+++ server/server.go
@@ -0,0 +1,86 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+const JsonContentType = "application/json"
+
+// Player stores a name with a number of wins
+type Player struct {
+	Name string
+	Wins int
+}
+
+// PlayerStore stores scope information about players.
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+	RecordWin(name string)
+	GetLeague() []Player
+}
+
+// PlayerServer is a HTTP interface for player information.
+type PlayerServer struct {
+	Store PlayerStore
+	http.Handler
+}
+
+// NewPlayerServer creates a PlayerServer with routing configured.
+func NewPlayerServer(store PlayerStore) *PlayerServer {
+	p := new(PlayerServer)
+
+	p.Store = store
+
+	router := http.NewServeMux()
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playerHandler))
+
+	p.Handler = router
+
+	return p
+}
+
+func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", JsonContentType)
+	json.NewEncoder(w).Encode(p.Store.GetLeague())
+}
+
+func (p *PlayerServer) playerHandler(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	score := p.Store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.Store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func GetPlayerScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+	return ""
+}
